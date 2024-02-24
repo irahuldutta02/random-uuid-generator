8a944d8d922714fa1df9826512a9dcb8644472b1
irahuldutta02 <78687135+irahuldutta02@users.noreply.github.com>
2024-02-24 22:51:12 +0530

    changes style and copy regenerate logic changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,39 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import toast, { Toaster } from "react-hot-toast";
 import "./App.css";
 
 function App() {
   const [uuid, setUuid] = useState(crypto.randomUUID());
-  const [message, setMessage] = useState("");
-  const [messageColor, setMessageColor] = useState("text-green-400");
 
-  function handleGenerateUUID() {
+  function generateNewUUID() {
     setUuid(crypto.randomUUID());
-    setMessageColor("bg-orange-400");
-    setMessage("New UUID Generated");
   }
+  const notify = (message) =>
+    toast.success(message, {
+      duration: 1000,
+      position: "bottom-center",
+    });
 
   function handleCopyID() {
+    toast.dismiss();
+    generateNewUUID();
     navigator.clipboard.writeText(uuid);
-    setMessageColor("bg-green-400");
-    setMessage("UUID Copied to Clipboard");
+    notify("UUID copied to clipboard");
   }
-
-  useEffect(() => {
-    if (message.length > 0) {
-      const timer = setTimeout(() => {
-        setMessage("");
-      }, 1000);
-      return () => {
-        clearTimeout(timer);
-      };
-    }
-  }, [message]);
 
   return (
     <>
-      <div className="flex flex-col items-center justify-center h-screen bg-gray-900 text-white text-center p-4">
-        <div className="mb-8">
-          <h1 className="text-4xl font-bold">Random UUID Generator</h1>
-        </div>
-        <div className="flex items-center space-x-4 justify-center flex-col gap-8">
+      <div className="flex flex-col items-center justify-center h-screen text-white text-center p-4 w-full">
+        <div className="flex items-center space-x-4 justify-center flex-col gap-8 bg-glass p-4 border-2 border-white rounded-lg w-full max-w-lg ">
+          <div className="flex justify-center items-center text bg-center">
+            <h1 className="text-4xl font-bold pb-8">UUID Generator</h1>
+          </div>
+
           <div className="flex items-center space-x-2 border-2 border-white p-2 rounded-md">
             <span className="text-xl">{uuid}</span>
           </div>
 
           <div className="flex justify-center items-center gap-4 text-sm">
-            <button
-              className="px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-600"
-              onClick={handleGenerateUUID}
-            >
-              Generate UUID
-            </button>
             <button
               className="px-4 py-2 text-white bg-green-500 rounded hover:bg-green-600"
               onClick={handleCopyID}
@@ -54,19 +41,12 @@ function App() {
                 e.target.style.transform = "scale(1.05)";
               }}
             >
-              Copy
+              Copy and Regenerate
             </button>
           </div>
-
-          {message.length > 0 && (
-            <div
-              className={`fixed bottom-4 right-4 px-4 py-2 ${messageColor} rounded-md`}
-            >
-              <span className="text-black">{message}</span>
-            </div>
-          )}
         </div>
       </div>
+      <Toaster />
     </>
   );
 }
